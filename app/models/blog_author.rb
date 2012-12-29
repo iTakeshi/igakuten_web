f3717838547d0bda9e11224b0978a21475ae032b
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-12-29 20:18:18 +0900

    BlogAuthore validation

--- app/models/blog_author.rb
+++ app/models/blog_author.rb
@@ -1,3 +1,9 @@
+# coding: utf-8
+
 class BlogAuthor < ActiveRecord::Base
   attr_accessible :name
+
+  validates :name,
+    presence: {message: '名前を入力してください。'},
+    uniqueness: {message: '入力された名前はすでに使用されています。'}
 end
