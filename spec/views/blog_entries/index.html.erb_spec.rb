22333b86f74eff5b71d66d8d7d0ee0555f7e6bf6
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-12-30 23:33:06 +0900

    BlogEntries#index修正

--- spec/views/blog_entries/index.html.erb_spec.rb
+++ spec/views/blog_entries/index.html.erb_spec.rb
@@ -2,16 +2,22 @@ require 'spec_helper'
 
 describe "blog_entries/index" do
   before(:each) do
+    blog_author = BlogAuthor.create!(name: 'John')
+    time = Time.now
     assign(:blog_entries, [
       stub_model(BlogEntry,
         :title => "Title",
         :unsafe_html => "MyText",
-        :blog_author_id => 1
+        :blog_author_id => blog_author.id,
+        :created_at => time,
+        :updated_at => time + 1
       ),
       stub_model(BlogEntry,
         :title => "Title",
         :unsafe_html => "MyText",
-        :blog_author_id => 1
+        :blog_author_id => blog_author.id,
+        :created_at => time,
+        :updated_at => time
       )
     ])
   end
@@ -19,8 +25,10 @@ describe "blog_entries/index" do
   it "renders a list of blog_entries" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    assert_select "section h2", :text => "Title".to_s, :count => 2
+    assert_select "section article", :text => "MyText".to_s, :count => 2
+    assert_select "section span.blog-author", :text => "John".to_s, :count => 2
+    assert_select "section span.blog-date", :text => /^\d{4}\.\d{2}\.\d{2}$/, :count => 1
+    assert_select "section span.blog-date", :text => /^\d{4}\.\d{2}\.\d{2} \/ \d{4}\.\d{2}\.\d{2}$/, :count => 1
   end
 end
