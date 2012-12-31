7a2881b9116a501155fb22bda7731cbe88d22f92
iTakeshi <takeshi.ito.doraemon@gmail.com>
2012-12-31 20:48:55 +0900

    authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  if APP_CONFIG['login']['required']
+    http_basic_authenticate_with name: APP_CONFIG['login']['user'], password: APP_CONFIG['login']['pass']
+  end
 end
