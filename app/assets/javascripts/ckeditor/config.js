8a593506f57bc4171eb4fd5fd27a4e51362dcd18
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-01-01 16:12:02 +0900

    ckeditorの設定をviewに移動

--- app/assets/javascripts/ckeditor/config.js
+++
@@ -1,9 +0,0 @@
-CKEDITOR.editorConfig = function( config )
-{
-    config.toolbar =
-    [
-        [ 'Source' ],
-        [ 'Bold', 'Italic', 'Underline', 'Strike', '-', 'NumberedList', 'BulletedList', '-', 'Link', 'Image', 'Table', 'HorizontalRule', 'Smiley' ],
-        [ 'Format' ]
-    ];
-};
