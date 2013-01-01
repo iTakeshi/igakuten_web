777100887014c6ca24f0c655e29a423bf60ef949
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-01-01 15:44:51 +0900

    authenticateをfilterに

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  if APP_CONFIG['login']['required']
-    http_basic_authenticate_with name: APP_CONFIG['login']['user'], password: APP_CONFIG['login']['pass']
+  before_filter :authenticate if APP_CONFIG['login']['required']
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == APP_CONFIG['login']['user'] && pass == APP_CONFIG['login']['pass']
+    end
   end
 end
