49a66a1c3758e9f72503860ad16ae39b47f78cf6
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-02-02 19:51:52 +0900

    footer contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def footer_nav(toc)
-    html_str = "<div><h1><a href=\"#{toc[1]}\">#{toc[0]}</a></h1><ul>"
+    html_str = "<div><h2><a href=\"#{toc[1]}\">#{toc[0]}</a></h2><ul>"
     if toc[2]
       toc[2].each do |k, v|
         html_str << "<li><a href=\"#{toc[1] + v}\">#{k}</a></li>"
