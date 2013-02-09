2f5ee25b8b201a233237fa6f15754ece33ef43e2
iTakeshi <takeshi.ito.doraemon@gmail.com>
2013-02-09 10:28:42 +0900

    debug footer helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     html_str = "<div><h2><a href=\"#{toc[1]}\">#{toc[0]}</a></h2><ul>"
     if toc[2]
       toc[2].each do |k, v|
-        html_str << "<li><a href=\"#{toc[1] + v}\">#{k}</a></li>"
+        html_str << "<li><a href=\"#{toc[1]}##{v}\">#{k}</a></li>"
       end
     end
     html_str << "</ul></div>"
